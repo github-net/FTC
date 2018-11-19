ab1c57f2b7e8b9280d5710ae767957e2de0f078a
Near321 <35989617+Near321@users.noreply.github.com>
2018-11-18 19:01:49 -0800

    Update LeTHIRDAvaJopMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/LeTHIRDAvaJopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/LeTHIRDAvaJopMode.java
@@ -1,29 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
-//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.Gyroscope;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-/**
- THIS IS THE ITERATIVE JAVA OP MODE
- working version i think
- */
 
-@TeleOp(name="Le THIRD AvA jOp Mode", group="Iterative Opmode")
-//@Disabled
-public class LeTHIRDAvaJopMode extends OpMode
-{
-    // Declare OpMode members.
+
+@TeleOp
+public class MySecondJavaOpMode extends LinearOpMode {
+
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
@@ -31,142 +19,70 @@ public class LeTHIRDAvaJopMode extends OpMode
     private CRServo capturing = null;
     private DcMotor lift = null;
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
-    @Override
-    public void init() {
-        telemetry.addData("Le Status", "is very Initialization");
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
+    DcMotor left;
+    DcMotor right;
+
+    @Override
+    public void runOpMode() {
+        //imu = hardwareMap.get(Gyroscope.class, "imu");
+        //DcMotor motorTest = hardwareMap.get(DcMotor.class, "motorTest");
+        //DigitalChannel digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+        //DistanceSensor sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+        //Servo servoTest = hardwareMap.get(Servo.class, "servoTest");
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-        arm        = hardwareMap.get(DcMotor.class, "arm");
-        capturing = hardwareMap.get(CRServo.class, "capturing");
+        arm = hardwareMap.get(DcMotor.class,"arm");
+        capturing = hardwareMap.get(CRServo.class,"capturing");
         lift = hardwareMap.get(DcMotor.class, "lift");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
-        arm.setDirection(DcMotor.Direction.FORWARD);
-
-        // Tell the driver that initialization is complete.
-        telemetry.addData("Le Status", "is very Initialization");
-    }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
-
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
         runtime.reset();
+
+        while (opModeIsActive()) {
+            double leftPower;
+            double rightPower;
+            //change the negative
+            boolean lift_button = gamepad2.x;
+            boolean lift_button_reverse = gamepad2.y
+            boolean button_press = gamepad2.a;
+            boolean reverse_button_press = gamepad2.b;
+            double drive = gamepad1.left_stick_y;
+            double turn  = -gamepad1.right_stick_x;
+            leftPower = Range.clip(drive + turn, -0.5, 0.5) ;
+            rightPower = Range.clip(drive - turn, -0.5, 0.5) ;
+            capturing.setPower(200);
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+            if (button_press == true) {
+                arm.setPower(500);
+                sleep(250);
+                arm.setPower(0);
+            }
+            if (reverse_button_press == true) {
+                arm.setPower(-500);
+                sleep(250);
+                arm.setPower(0);
+            }
+            if (lift_button == true) {
+                lift.setPower(1500);
+                sleep(250);
+                arm.setPower(0);
+            }if (lift_button_reverse == true) {
+                lift.setPower(-1500);
+                sleep(250);
+                arm.setPower(0);
+            }
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.update();
+        // run until the end of the match (driver presses STOP)
+
+        }
     }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
-    @Override
-    public void loop() {
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
-        boolean button_up = gamepad1.a;
-        boolean button_down = gamepad1.b;
-        boolean button_press = gamepad1.x;
-        boolean reverse_button_press = gamepad1.y;
-
-        double drive =  -gamepad1.left_stick_y; //moving forward and backward
-        double turn  =  gamepad1.left_stick_x; //turn left and right
-
-
-
-        leftPower    = Range.clip(drive + turn, -0.5, 0.5) ;
-        rightPower   = Range.clip(drive - turn, -0.5, 0.5) ;
-
-
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        int time;
-        time = 250;
-        /**
-         * LIFT
-         */
-        if (button_press == true) {
-            lift.setPower(1000);
-            //try and catch code
-            try{
-                java.lang.Thread.sleep(time);
-            }catch(InterruptedException ie){
-
-            }
-            //end of try and catch code
-            lift.setPower(0);
-        }
-        if (reverse_button_press == true) {
-            lift.setPower(-1000);
-            //try and catch code
-            try{
-                java.lang.Thread.sleep(time);
-            }catch(InterruptedException ie){
-
-            }
-            //end of try and catch code
-            lift.setPower(0);
-            /**
-             * ARM
-             */
-        }
-        if (button_up == true) {
-            arm.setPower(1000);
-            //try and catch code
-            try{
-                java.lang.Thread.sleep(time);
-            }catch(InterruptedException ie){
-
-            }
-            //end of try and catch code
-            arm.setPower(0);
-        }
-        if (button_down == true) {
-            arm.setPower(-1000);
-            //try and catch code
-            try{
-                java.lang.Thread.sleep(time);
-            }catch(InterruptedException ie){
-
-            }
-            //end of try and catch code
-            arm.setPower(0);
-        }
-
-        // Send calculated power to wheels
-        capturing.setPower(200);
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
-
-
-
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Le StAtUs", "Le InitIaliZation TiMe: " + runtime.toString());
-        telemetry.addData("Le MoToRs", "Left Motor Power: (%.2f) Right Motor Power (%.2f)", leftPower, rightPower);
-    }
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
-    }
-
 }
