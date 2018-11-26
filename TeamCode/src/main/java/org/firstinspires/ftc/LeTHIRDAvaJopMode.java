da9c715569ff2413a28b89f56ad18497993d6da1
j fan <39718024+github-net@users.noreply.github.com>
2018-11-25 17:28:52 -0800

    Update LeTHIRDAvaJopMode.java
    adjusted for four wheel drive

--- TeamCode/src/main/java/org/firstinspires/ftc/LeTHIRDAvaJopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/LeTHIRDAvaJopMode.java
@@ -15,20 +15,23 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 /**
- THIS IS THE ITERATIVE JAVA OP MODE
- working version i think
+
+ working version
  */
 
 @TeleOp(name="Le THIRD AvA jOp Mode", group="Iterative Opmode")
 //@Disabled
 public class LeTHIRDAvaJopMode extends OpMode
 {
-    // Declare OpMode members.
+    // Declare motors and servos
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
+    private DcMotor back_leftDrive = null;
+    private DcMotor back_rightDrive = null;
     private DcMotor arm = null;
     private CRServo capturing = null;
+    //private Servo grip = null;
     private DcMotor lift = null;
 
     /*
@@ -43,9 +46,12 @@ public class LeTHIRDAvaJopMode extends OpMode
         // step (using the FTC Robot Controller app on the phone).
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        back_leftDrive  = hardwareMap.get(DcMotor.class, "back_left_drive");
+        back_rightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
         arm        = hardwareMap.get(DcMotor.class, "arm");
         capturing = hardwareMap.get(CRServo.class, "capturing");
         lift = hardwareMap.get(DcMotor.class, "lift");
+        //grip = hardwareMap.get(Servo.class, "grip");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
@@ -67,18 +73,27 @@ public class LeTHIRDAvaJopMode extends OpMode
         // Setup a variable for each drive wheel to save power level for telemetry
         double leftPower;
         double rightPower;
+        double backleftPower;
+        double backrightPower;
         boolean button_up = gamepad2.a;             //a and b for arm
         boolean button_down = gamepad2.b;
         boolean button_press = gamepad2.x;          //x and y for lift
         boolean reverse_button_press = gamepad2.y;
+        //boolean button_grip_close = gamepad2.left_bumper; //grip close
+        //boolean button_grip_open = gamepad2.right_bumper; //grip open
 
         float capturing_power = gamepad2.left_trigger; //left trigger for capturing power
+        double back_drive = gamepad1.right_stick_x; //back drive
+        double back_turn = -gamepad1.right_stick_y; //back turn
+        double drive =  -gamepad1.left_stick_y; //front drive
+        double turn  =  gamepad1.left_stick_x; //front turn
 
-        double drive =  gamepad1.left_stick_y; //moving forward and backward
-        double turn  =  gamepad1.left_stick_x; //turn left and right
 
-        leftPower    = Range.clip(drive + turn, -0.5, 0.5) ;
-        rightPower   = Range.clip(drive - turn, -0.5, 0.5) ;
+        leftPower        = Range.clip(drive + turn, -0.75, 0.75) ;
+        rightPower       = Range.clip(drive - turn, -0.75, 0.75) ;
+        backleftPower    = Range.clip(back_drive + back_turn, -1.0, 1.0) ;
+        backrightPower   = Range.clip(back_drive - back_turn, -1.0, 1.0) ;
+
 
         capturing.setPower(capturing_power * -200); //capturing power
 
@@ -136,17 +151,29 @@ public class LeTHIRDAvaJopMode extends OpMode
             //end of try and catch code
             arm.setPower(0);
         }
+        /**
+         * GRIP NOT USED ANYMORE
 
+        if (button_grip_open == true) {
+            grip.setPosition(100); // Grip Open
+        }
+        if (button_grip_close == true) {
+            grip.setPosition(0); // Grip Close
+        }
+        */
         // Send calculated power to wheels
 
         leftDrive.setPower(leftPower);
         rightDrive.setPower(rightPower);
+        back_leftDrive.setPower(backleftPower);
+        back_rightDrive.setPower(backrightPower);
 
 
 
-        // Show the elapsed game time and wheel power.
+        // Show the elapsed game time, wheel power, and capturing power
         telemetry.addData("Le StAtUs", "Le InitIaliZation TiMe: " + runtime.toString());
         telemetry.addData("Le MoToRs", "Left Motor Power: (%.2f) Right Motor Power (%.2f)", leftPower, rightPower);
+        telemetry.addData("Le BaCk MoToRs", "Back Left Motor Power: (%.2f) Back Right Motor Power (%.2f)", backleftPower, backrightPower);
         telemetry.addData("Le Capturing", "Capturing Power: (%.2f)", capturingPower);
     }
 
